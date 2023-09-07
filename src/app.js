d1e3f2fb6b64ebd999e12f8c81566cafa7fe1e96
MilenaMed <115955806+MilenaMed@users.noreply.github.com>
2023-09-07 19:08:05 -0300

    initial settings
    -The port used by your server must be 5000.
    
    -Add the code that starts the server, with the listen function, in the src/app.js file.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import router from "./routers/indexRouter.js";
+import dotenv from "dotenv"
+
+const app = express();
+
+//Ferramentas
+app.use(express.json())
+app.use(router)
+dotenv.config()
+
+
+//Porta
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
