20143738a57c4eb1d2b98300d99d90651e05ac21
MilenaMed <115955806+MilenaMed@users.noreply.github.com>
2023-09-11 00:33:57 -0300

    GET  - /passengers/travels

--- src/controllers/travelsController.js
+++ src/controllers/travelsController.js
@@ -17,4 +17,42 @@ export async function postTravels(request, response) {
         return response.status(500).send(err.message)
     }
 };
+//GET - travels
+export async function getTravels(request, response) {
+    const { name } = request.query;
+
+    try {
+        if (!name) {
+            const allTravels = await db.query(`
+        SELECT CONCAT("firstName", ' ', "lastName") AS passenger,
+        COUNT(travels.id) AS travels
+        FROM passengers
+        LEFT JOIN travels ON passengers.id = travels."passengerId"
+        GROUP BY 
+        passengers.id, "firstName", "lastName"
+        ORDER BY COUNT(travels.id) DESC;
+         `);
+            return response.status(200).send(allTravels.rows);
+        }
+
+        const travelsQueryName = await db.query(`
+        SELECT CONCAT("firstName", ' ', "lastName") AS passenger,
+        COUNT(travels.id) AS travels
+        FROM passengers
+        LEFT JOIN travels ON passengers.id = travels."passengerId"
+        WHERE "firstName" ILIKE $1 OR "lastName" ILIKE $1
+        GROUP BY passengers.id, "firstName", "lastName"
+        ORDER BY COUNT(travels.id) DESC;
+        `, [`%${name}%`]);
+
+        if (travelsQueryName.rows.length > 10) {
+            return response.status(500).send('Too many results');
+        }
+        
+        return response.status(200).send(travelsQueryName.rows);
+
+    } catch (err) {
+        return response.status(500).send(err.message)
+    }
+};
 
--- src/routers/travelRouter.js
+++ src/routers/travelRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { validateMiddleware } from "../middlewares/validateMiddleware.js";
 import { travelSchema } from "../scheemas/travelsShema.js";
-import { postTravels } from "../controllers/travelsController.js";
+import { postTravels, getTravels } from "../controllers/travelsController.js";
 
 const travelsRouter = Router()
 
 travelsRouter.post("/travels", validateMiddleware(travelSchema),postTravels)
+travelsRouter.get("/passengers/travels", getTravels)
+
 
 export default travelsRouter
